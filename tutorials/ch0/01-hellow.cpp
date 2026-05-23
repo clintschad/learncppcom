456ff6a0a90e1f2a8c5ce8dcb25df5dbc258cb95
Clint Schad <clint.schad@icloud.com>
2026-05-23 08:29:12 -0400

    C++ compiler and debugger setup in VScode.

---
+++ tutorials/ch0/01-hellow.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "my dawg" << endl;
+    int a, b;
+    a = 3;
+    b = 2;
+
+    cout << "Hello world! a + b = " << (a + b) << endl;
+
+    return 0;
+}
